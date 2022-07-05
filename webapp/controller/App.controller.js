0ab99b816c9b04533d93359d29a01d180095c1a2
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-07-04 20:28:08 -0500

    modules test commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,13 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.App", {
-    onShowHello: function () {
-      //show native vanilla js alert
-      alert("hello There! SAPUI5");
-    },
-  });
-});
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.App", {
+      onShowHello: function () {
+        //show native vanilla js alert
+        //alert("hello There! SAPUI5");
+        MessageToast.show("Hello there messagetoast!");
+      },
+    });
+  }
+);
