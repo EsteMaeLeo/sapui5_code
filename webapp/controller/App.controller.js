40d79c7bd2f72277973e524bb253bc1f6fc3fec0
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-07-08 00:47:55 -0500

    json model ui5

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,27 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.App", {
+      //show native vanilla js alert
+      //alert("hello There! SAPUI5");
+
+      onInit: function () {
+        //set data model in the view
+        var oData = {
+          recipient: {
+            name: "UI5",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+      },
       onShowHello: function () {
-        //show native vanilla js alert
-        //alert("hello There! SAPUI5");
-        MessageToast.show("Hello there messagetoast!");
+        MessageToast.show("Hello there MessageToast!");
       },
     });
   }
